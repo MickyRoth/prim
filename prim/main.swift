13aa032b1a5e82b4145d98134578f2143183aa93
Michael Roth <rothnet@me.com>
2020-09-21 19:50:52 +0200

    1.0

--- prim/main.swift
+++ prim/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+var end : Int
+var prim : Bool
 
+print("Enter from>", terminator:"")
+let a = Int(readLine()!)!
+
+print("Enter to>", terminator:"")
+let b = Int(readLine()!)!
+
+print("Searching primes from",a,"to",b)
+for i in a...b {
+    prim = true
+    end=Int(sqrt(Float(i)))
+    for j in 2...end {
+        if i % j == 0 {
+            prim = false
+            break
+        }
+    }
+    if prim {print(i)}
+}
